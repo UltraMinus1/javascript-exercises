dc79b36f51b3dbb2a0e2ffc1acf6df3ade8e4a4e
Michal Szczegielniak <ultraminus11@gmail.com>
2024-06-24 18:57:32 +0200

    compleated exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    let removed =[];
+    for (let i=0; i<arr.length; i++) {
+        if (!elements.includes(arr[i])) {
+            removed.push(arr[i]);
+        }
+    }
+    return removed;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
