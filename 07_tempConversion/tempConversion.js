aa599ed48297961a5e353ac21cffc72ca39f1069
Michal Szczegielniak <ultraminus11@gmail.com>
2024-06-24 21:30:44 +0200

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(temp) {
+  return roundToDec((temp * 9)/5 + 32)
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(temp) {
+  return roundToDec(((temp - 32) * 5)/9)
 };
+
+function roundToDec(num) {
+  return Math.round(num * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
