1fe8effb5c8e6c2c83ccacdbd35ea9cae678bccd
Michal Szczegielniak <ultraminus11@gmail.com>
2024-06-28 19:21:10 +0200

    finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const alphanumeric = '1234567890qwertyuiopasdfghjklzxcvbnm'
+const palindromes = function (str) {
+    let cleanStr = str
+    .toLowerCase()
+    .split('')
+    .filter((char) => {
+        return alphanumeric.includes(char);
+    })
+    .join('')
 
+    let strReversed = cleanStr.split('').reverse().join('');
+    return strReversed == cleanStr;
 };
 
 // Do not edit below this line
