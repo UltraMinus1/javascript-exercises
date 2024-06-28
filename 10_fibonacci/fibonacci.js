8c7ff38a9997eb0f6ce14b443baad0ef5e0bba4b
Michal Szczegielniak <ultraminus11@gmail.com>
2024-06-28 19:34:12 +0200

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    let previous = 1;
+    let current = 0;
+    let h = 0;
+    for (let i = 1; i <= n; i++) {
+        h = current;
+        current += previous;
+        previous = h;
+    }
+    return current;
 };
+
+console.log(fibonacci(2))
 
 // Do not edit below this line
 module.exports = fibonacci;
