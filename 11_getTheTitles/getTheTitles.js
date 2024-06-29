216e50ea3e6f4f4e3fc2b9573a7dc28382bb0683
Michal Szczegielniak <ultraminus11@gmail.com>
2024-06-29 11:43:53 +0200

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookArr = [];
+    books.forEach((book) => {
+        bookArr.push(book['title'])
+    });
+    return bookArr;
 };
 
 // Do not edit below this line
