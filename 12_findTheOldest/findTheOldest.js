ab2d5a229e6b9596f3caad96b9513d5378d68954
Michal Szczegielniak <ultraminus11@gmail.com>
2024-06-29 12:09:22 +0200

    finished exercise 12 final

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const currentYear = new Date().getFullYear();
+const findTheOldest = function(people) {
+    let ages = people.map((obj) => {
+        if ('yearOfDeath' in obj) {
+            return obj.yearOfDeath - obj.yearOfBirth;
+        } else {
+            return currentYear - obj.yearOfBirth;
+        }
+        
+    });
+    //console.log(typeof(ages))
+    let bigestI 
+    let bigestAge = 0
+    for (let i = 0; i < ages.length; i++) {
+        if (bigestAge < ages[i]) {
+            bigestAge = ages[i];
+            bigestI = i;
+        }
+    }
+    return people[bigestI]
 };
 
 // Do not edit below this line
